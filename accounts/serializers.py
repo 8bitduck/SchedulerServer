054fc5cee15d9704c9085188105a4982e93b809f
Ryan Ploetz <ryan.ploetz@gmail.com>
2015-03-17 10:43:25 -0500

    removing project, about to upload the new one.

--- accounts/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import *
-
-class UserSerializer(serializers.ModelSerializer):
-	mobile_number = serializers.CharField(required=False, allow_null=True, max_length=10)
-
-	class Meta:
-		model = User
-		fields = ('mobile_number')
