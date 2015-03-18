4d62a02abd1bfe8ce2c3bf436b79890703b04c8d
Ryan Ploetz <ryan.ploetz@gmail.com>
2015-03-18 01:28:41 -0500

    schedules correctly and calls back to Parse

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Bottle App
 
 The scheduling server allows a user to schedule an SMS/MMS message to be sent to a phone list of the users choosing. This server is accomplied by a native iOS Application. 
 
-This server's only purpose is to schedule messages and send the identifier of the schedule (stored in Parse) back to the Parse servers. Parse handles the user authentication and storage of the data. This server has a SECRET_KEY that is stored only in the Parse Server and this Server to validate who the requests are coming from. If Parse sends the right SECRET_KEY, then the request goes through and schedules the message.
+This server's only purpose is to schedule messages and send the identifier of the schedule (stored in Parse) back to the Parse servers. Parse handles the user authentication and storage of the data. This server has a SECRET_KEY that is stored only in the Parse Server and this Server to validate who the requests are coming from. If Parse sends the right SECRET_KEY, then the request goes through and schedules the message
 
 
 ## Setup
--- server.py
+++ server.py
@@ -4,9 +4,11 @@ from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 
 import requests
 import json
+from pytz import utc
+import dateutil.parser
 
-# JOBS_DATABASE = "postgresql://ubuntu:ubuntu@localhost:5432/SMSSchedulerServer_dev"
-JOBS_DATABASE = "postgresql://vagrant:vagrant@localhost:5432/SMSSchedulerServer_dev"
+JOBS_DATABASE = "postgresql://ubuntu:ubuntu@localhost:5432/SMSSchedulerServer_dev"
+# JOBS_DATABASE = "postgresql://vagrant:vagrant@localhost:5432/SMSSchedulerServer_dev"
 
 SECRET_KEY = "h*|.yUKf37#JBhPCvNI7NAlWLa@:Fqz-GUHK%4H==mU B0+vyWyo*#h{Rgj6nSsb"
 
@@ -16,46 +18,64 @@ PARSE_API = "https://api.parse.com/1/functions/"
 
 app = Bottle()
 
+
 def trigger_parse_cloud_function(endpoint, payload):
-	headers = {
-		'X-Parse-Application-Id' : PARSE_APP_ID,
-		'X-Parse-REST-API-Key' : PARSE_REST_API_KEY,
-		'Content-Type' : 'application/json'
-	}
+    headers = {
+        'X-Parse-Application-Id': PARSE_APP_ID,
+        'X-Parse-REST-API-Key': PARSE_REST_API_KEY,
+        'Content-Type': 'application/json'
+    }
 
-	url = PARSE_API + endpoint
-	r = requests.post(url, data=json.dumps(payload), headers=headers)
-	print(r.text)
+    url = PARSE_API + endpoint
+    r = requests.post(url, data=json.dumps(payload), headers=headers)
+    print(r.text)
 
-def trigger_message():
-	print('trigger_message')
-	payload = {
-		'phone' : '5129343410',
-		'body' : 'test from bottle'
-	}
-	trigger_parse_cloud_function('sendSms', payload)
+
+def trigger_message(**kwargs):
+    print('trigger_message')
+    payload = {
+        'message_id': kwargs['message_id']
+    }
+    trigger_parse_cloud_function('sendSms', payload)
+
+# Schedule the message on the server with the message_id that is on Parse
+
 
 @app.route('/message', method='POST')
 def create_message():
+    print('/message')
+    print(request.json.get('message_id'))
+    print(request.json.get('timestamp'))
+
     secret_key = request.json.get('SECRET_KEY')
     response = dict()
 
     if secret_key != SECRET_KEY:
-    	response["response"] = "error"
-    	response["error"] = "Invalid Key"
+        response["response"] = "error"
+        response["error"] = "Invalid Key"
     else:
-    	# Need a timestamp of when to schedule
-    	# Need a cloud function to call
-    	# Need an ID to pass back to Parse
-    	# Store in a database table the ID
-    	scheduler = BackgroundScheduler()
-    	scheduler.add_jobstore(SQLAlchemyJobStore(url=JOBS_DATABASE, tablename='apscheduler_jobs'), 'default')
-    	job = scheduler.add_job(trigger_message, 'date', run_date=request.json.get('timestamp'))
-    	scheduler.start()
+        # Need a timestamp of when to schedule
+        # Need a cloud function to call
+        # Need an ID to pass back to Parse
+        # Store in a database table the ID
+        scheduler = BackgroundScheduler()
+        scheduler.add_jobstore(SQLAlchemyJobStore(url=JOBS_DATABASE,
+                                                  tablename='apscheduler_jobs'),
+                               'default')
 
-    	response["response"] = "success"
-    	response["job_id"] = job.id
+        payload = {
+            'message_id': request.json.get('message_id')
+        }
 
+        job = scheduler.add_job(trigger_message,
+                                'date',
+                                run_date=dateutil.parser.parse(
+                                    request.json.get('timestamp')),
+                                kwargs=payload)
+        scheduler.start()
+
+        response["response"] = "success"
+        response["job_id"] = job.id
     return response
 
 run(app, server='gunicorn', host='0.0.0.0', port=8080)
